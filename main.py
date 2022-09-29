b167980e736d51fce5cbfb7086327a562b7de0d9
RobsenGelana <robsengelana21@gmail.com>
2022-09-29 21:33:32 +0300

    updating user interface

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from cgitb import text
 from tkinter import *
 import pandas as pd
 import time
@@ -7,9 +8,11 @@ BACKGROUND_COLOR = "#B1DDC6"
 #-----------------READING CSV FILE ------------------------#
 df = pd.read_csv('data/french_words.csv')
 to_learn = df.to_dict(orient='records')
-print(to_learn)
 def random_word():
-   pass
+   current_card = random.choice(to_learn)
+   canvas.itemconfig(card_title, text="French")
+   canvas.itemconfig(card_word, text=current_card['French'])
+   
 #-----------------------------------------------------------#
 #print(f'{random_word()}')
 
@@ -20,8 +23,8 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 canvas = Canvas(height=526, width=800)
 card_front_img = PhotoImage(file='images/card_front.png')
 canvas.create_image(400, 263, image=card_front_img)
-en = canvas.create_text(400, 150, text="Title", font=('Ariel', 40, 'normal'))
-fr = canvas.create_text(400, 263, text=f"word", font=('Ariel', 60, 'normal'))
+card_title = canvas.create_text(400, 150, text="Title", font=('Ariel', 40, 'normal'))
+card_word = canvas.create_text(400, 263, text=f"word", font=('Ariel', 60, 'normal'))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(column=0, row=0, columnspan=2)
 
@@ -34,7 +37,7 @@ wrong = Button(image=wrong_image, highlightthickness=0, bg=BACKGROUND_COLOR, com
 wrong.grid(row=1, column=0)
 
 
-
+random_word()
 
 
 
